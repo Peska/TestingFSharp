ca7e91c8d669d7e8d51fdf361d0adeb485953cb7
Peska <peska_jr@o2.pl>
2016-10-22 18:47:26 +0200

    more examples

--- TestingFSharp/Program.fs
+++ TestingFSharp/Program.fs
@@ -1,7 +1,11 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿module TestingFSharpModule
+
+let square x = x * x
+let isOdd x = x % 2 <> 0
 
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
+    printfn "%d squared is: %d" 12 (square 12)
+    printfn "%d isOdd? Answer: %b" 12 (isOdd 12)
+    printfn "%d squared isOdd? Answer: %b" 12 (12 |> square |> isOdd)
     0 // return an integer exit code
